c5e113dd01de33405a710fd36fc83207b4704e23
Stevie <steviepinero@gmail.com>
2016-06-20 11:21:15 -0400

    Modified parser to include all feeds

--- lib/opml_saw/parser.rb
+++ lib/opml_saw/parser.rb
@@ -1,26 +1,26 @@
 module OpmlSaw
-    
+
   class Parser
-    
+
     attr_reader :feeds
-    
+
     def initialize(xml)
       @xml = xml
       @feeds = []
     end
-  
+
     def document
       @document ||= Nokogiri::XML.parse(@xml).css("body").children
     end
-  
+
     def has_children?(node)
       node.children.length > 0
     end
-  
+
     def is_outline?(node)
       'outline' == node.name
     end
-  
+
     def parse(data = nil, tag = nil)
       data ||= document
       data.each do |node|
@@ -29,12 +29,13 @@ module OpmlSaw
           if has_children?(node)
             title = outline[:title] || outline[:text]
             parse(node.children, title)
-          else
-            @feeds << outline
+          #else
+
           end
+          @feeds << outline
         end
       end
     end
-  
+
   end
 end
